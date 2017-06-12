95b70712dae648d13ab206353c24de70ce363f06
David Castelblanco Benavides <cbdavides@gmail.com>
2017-06-12 00:38:36 -0500

    Se implementa la acción para capturar polígonos.

--- src/visor/VisorShapefiles.py
+++ src/visor/VisorShapefiles.py
@@ -9,7 +9,7 @@ from qgis.core import QgsVectorLayer, QgsVectorLayer, QgsMapLayerRegistry
 from qgis.gui import (QgsMapCanvas, QgsMapToolPan, QgsMapToolZoom, QgsMapCanvasLayer)
 
 from WindowUI import Ui_MainWindow
-from tools import PointMapTool
+from tools import PointMapTool, PolyMapTool
 
 qgis_prefix = '/usr'
 
@@ -41,12 +41,16 @@ class VisorShapefiles(QMainWindow, Ui_MainWindow):
         self.actionMove = QAction(QIcon('images/mActionPan.svg'), u'Mover', self.frame)
         self.connect(self.actionMove, SIGNAL('activated()'), self.pan)
 
-        self.actionZoomFull = QAction(QIcon('images/mActionZoomFullExtent.svg'), u'Vista completa', self.frame)
+        self.actionZoomFull = QAction(QIcon('images/mActionZoomFullExtent.svg'), u'Vista Completa', self.frame)
         self.connect(self.actionZoomFull, SIGNAL('activated()'), self.zoomFull)
 
-        self.actionPoint = QAction(QIcon('images/mActionCapturePoint.svg'), u'Capturar punto', self.frame)
+        self.actionPoint = QAction(QIcon('images/mActionCapturePoint.svg'), u'Capturar Punto', self.frame)
         self.actionPoint.setCheckable(True)
         self.connect(self.actionPoint, SIGNAL('triggered()'), self.point)
+
+        self.actionPoly = QAction(QIcon('images/mActionCapturePolygon.svg'), u'Capturar Polígono', self.frame)
+        self.actionPoly.setCheckable(True)
+        self.connect(self.actionPoly, SIGNAL('triggered()'), self.poly)
 
         self.toolbar = self.addToolBar('Map')
         self.toolbar.addAction(self.actionAddLayer)
@@ -55,15 +59,21 @@ class VisorShapefiles(QMainWindow, Ui_MainWindow):
         self.toolbar.addAction(self.actionMove)
         self.toolbar.addAction(self.actionZoomFull)
         self.toolbar.addAction(self.actionPoint)
+        self.toolbar.addAction(self.actionPoly)
 
         self.toolPan = QgsMapToolPan(self.canvas)
         self.toolZoomIn = QgsMapToolZoom(self.canvas, False)
         self.toolZoomOut = QgsMapToolZoom(self.canvas, True)
         self.toolPoint = PointMapTool(self.canvas)
+        self.toolPoly = PolyMapTool(self.canvas)
 
         self.toolPoint.setAction(self.actionPoint)
+        self.toolPoly.setAction(self.actionPoly)
 
         self.layers = []
+
+    def poly(self):
+        self.canvas.setMapTool(self.toolPoly)
 
     def point(self):
         self.canvas.setMapTool(self.toolPoint)
--- src/visor/tools.py
+++ src/visor/tools.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4.QtGui import QColor
-from qgis.gui import QgsMapToolEmitPoint, QgsVertexMarker
+from PyQt4.QtCore import Qt
+from qgis.gui import QgsMapToolEmitPoint, QgsVertexMarker, QgsRubberBand
+from qgis.core import QGis
 
 class PointMapTool(QgsMapToolEmitPoint):
 
@@ -27,3 +29,39 @@ class PointMapTool(QgsMapToolEmitPoint):
 
         self.m.setIconType(QgsVertexMarker.ICON_X)
         self.m.setPenWidth(3)
+
+class PolyMapTool(QgsMapToolEmitPoint):
+
+    def __init__(self, canvas):
+        self.canvas = canvas
+        QgsMapToolEmitPoint.__init__(self, self.canvas)
+
+        self.rubberband = QgsRubberBand(self.canvas, QGis.Polygon)
+        self.rubberband.setColor(Qt.red)
+        self.rubberband.setWidth(1)
+
+        self.point = None
+        self.points = []
+
+    def canvasPressEvent(self, e):
+        self.point = self.toMapCoordinates(e.pos())
+
+        m = QgsVertexMarker(self.canvas)
+        m.setCenter(self.point)
+        m.setColor(QColor(0, 255, 0))
+        m.setIconSize(5)
+        m.setIconType(QgsVertexMarker.ICON_BOX)
+        m.setPenWidth(3)
+
+        self.points.append(self.point)
+        self.isEmittingPoint = True
+        self.showPoly()
+
+    def showPoly(self):
+        self.rubberband.reset(QGis.Polygon)
+
+        for point in self.points[: -1]:
+            self.rubberband.addPoint(point, False)
+
+        self.rubberband.addPoint(self.points[-1], True)
+        self.rubberband.show()
