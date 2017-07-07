c8d56282aa442ca99db762872d8101a3ac0110b5
David Castelblanco Benavides <cbdavides@gmail.com>
2017-07-06 19:30:38 -0500

    se carga un shapefile, y se prepara un mapa de mapnik, para renderizar en la pantalla

--- src/visor/VisorShapefiles.py
+++ src/visor/VisorShapefiles.py
@@ -2,11 +2,65 @@
 
 import sys
 
-from PyQt4.QtCore import SIGNAL, QFileInfo
+from PyQt4.QtCore import SIGNAL, QFileInfo, QPoint, QByteArray
 from PyQt4.QtGui import QMainWindow, QColor, QIcon, QVBoxLayout, QAction, QFileDialog
-from PyQt4.QtGui import QApplication
+from PyQt4.QtGui import QApplication, QWidget, QImage
 
 from WindowUI import Ui_MainWindow
+
+import mapnik
+
+class MapnikWidget(QWidget):
+    def __init__(self, parent = None):
+        QWidget.__init__(self, parent)
+        self.map = mapnik.Map(256, 256)
+        self.qim = QImage()
+        #Drag positions
+        self.startDragPos = QPoint()
+        self.endDragPos = QPoint()
+        #Zoom poistion
+        self.zoomPos = QPoint()
+
+        self.drag = False
+        self.scale = False
+        self.total_scale = 1.0
+
+    #temp functions to try a few things
+    def createMap(self):
+        s = mapnik.Style()
+        r = mapnik.Rule()
+
+        polygon_symbolizer = mapnik.PolygonSymbolizer()
+        polygon_symbolizer.fill = mapnik.Color('#f2eff9')
+        r.symbols.append(polygon_symbolizer)
+
+        line_symbolizer = mapnik.LineSymbolizer()
+        line_symbolizer.stroke = mapnik.Color('rgb(50%, 50%, 50%)')
+        line_symbolizer.stroke_width = 0.1
+        r.symbols.append(line_symbolizer)
+
+        s.rules.append(r)
+
+        self.map.append_style('Estilo1', s)
+
+        #Loading source
+        data_source = mapnik.Shapefile(file = 'Colombia/Colombia.shp')
+        print data_source.envelope()
+
+        layer = mapnik.Layer('Colombia')
+        layer.datasource = data_source
+        layer.styles.append('Estilo1')
+
+        self.map.layers.append(layer)
+        self.map.zoom_all()
+
+        im = mapnik.Image(self.map.width, self.map.height)
+        mapnik.render(self.map, im)
+
+        self.qim.loadFromData(QByteArray(im.tostring('png')))
+        self.update()
+
+
 
 class VisorShapefiles(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -16,8 +70,17 @@ class VisorShapefiles(QMainWindow, Ui_MainWindow):
         self.setWindowTitle(u'Visor de Shapefiles')
 
         self.layout = QVBoxLayout(self.frame)
-        # self.layout.addWidget(self.canvas)
 
+        maper = MapnikWidget()
+        maper.createMap()
+
+        canvas = QCanvas()
+
+        self.layout.addWidget(maper)
+
+
+
+        # TODO: Move all the declarations of the actions
         self.actionAddLayer = QAction(QIcon('images/mActionAddOgrLayer.svg'), u'Agregar capa',self.frame)
         self.connect(self.actionAddLayer, SIGNAL('activated()'), self.addLayer)
 
