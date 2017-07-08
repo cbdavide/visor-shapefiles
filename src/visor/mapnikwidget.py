6ee18e7e1553b52e3fbb1c9cffa03433f055421a
David Castelblanco Benavides <cbdavides@gmail.com>
2017-07-08 00:02:07 -0500

    Se implementa la acción de zoom, y se el evento del click derecho del ratón traduce la posición del pixel en el mapa a la coordenda geográfica del mapa.

--- src/visor/mapnikwidget.py
+++ src/visor/mapnikwidget.py
@@ -1,8 +1,8 @@
 import sys
 
-from PyQt4.QtCore import SIGNAL, QFileInfo, QPoint, QByteArray, Qt
+from PyQt4.QtCore import SIGNAL, QFileInfo, QPoint, QByteArray, Qt, QRectF, QTimer
 from PyQt4.QtGui import QMainWindow, QColor, QIcon, QVBoxLayout, QAction, QFileDialog
-from PyQt4.QtGui import QApplication, QWidget, QImage, QPainter
+from PyQt4.QtGui import QApplication, QWidget, QImage, QPainter, QMatrix
 
 from WindowUI import Ui_MainWindow
 
@@ -20,6 +20,9 @@ class MapnikWidget(QWidget):
         #Zoom poistion
         self.zoomPos = QPoint()
 
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.updateMap)
+
         self.drag = False
         self.scale = False
         self.total_scale = 1.0
@@ -30,13 +33,44 @@ class MapnikWidget(QWidget):
         painter = QPainter(self)
         if self.drag:
             painter.drawImage(self.endDragPos - self.startDragPos, self.qim)
+        if self.scale:
+            painter.save()
+            scale = 1 / self.total_scale
+            painter.translate(self.zoomPos.x(), self.zoomPos.y())
+            painter.scale(scale, scale)
+            painter.translate(-self.zoomPos.x(), -self.zoomPos.y())
+            exposed = painter.matrix().inverted()[0].mapRect(self.rect())
+            painter.drawImage(exposed, self.qim, exposed)
+            painter.restore()
         else:
             painter.drawImage(0, 0, self.qim)
+
+        painter.setPen(QColor(0, 0, 0, 100))
+        painter.setBrush(QColor(0, 0, 0, 100))
+        painter.drawRect(0, 0, 256, 26)
+        painter.setPen(QColor(0, 255 , 0))
+        painter.drawText(10, 19, 'Scale Denominator: ' + str(self.map.scale_denominator()))
+
+    def wheelEvent(self, event):
+        self.zoomPos = event.pos()
+        self.total_scale *= 1.0 - event.delta() / (360.0 * 8.0) * 4
+        self.scale = True
+
+        self.update()
+        self.timer.start(400)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.startDragPos = event.pos()
             self.drag = True
+        else:
+            # transform = self.map.view_transform()
+            coord = self.map.view_transform().backward(mapnik.Coord(event.pos().x(), event.pos().y()))
+            print coord
+
+            print '>', event.pos().x(), event.pos().y()
+            # print self.map.envelope().center()
+            # print mapnik.Coord(event.pos().x(), event.pos().y())
 
     def mouseMoveEvent(self, event):
         if self.drag:
@@ -49,21 +83,31 @@ class MapnikWidget(QWidget):
             self.updateMap()
 
     def updateMap(self):
+        self.timer.stop()
+
         if self.drag:
             cx = int(0.5 * self.map.width)
             cy = int(0.5 * self.map.height)
             dpos = self.endDragPos - self.startDragPos
             self.map.pan(cx - dpos.x(), cy - dpos.y())
             self.drag = False
+        elif self.scale:
+            ma = QMatrix()
+            ma.translate(self.zoomPos.x(), self.zoomPos.y())
+            ma.scale(self.total_scale, self.total_scale)
+            ma.translate(-self.zoomPos.x(), -self.zoomPos.y())
+
+            rect = ma.mapRect(QRectF(0, 0, self.map.width, self.map.height))
+            env = mapnik.Envelope(rect.left(), rect.bottom(), rect.right(), rect.top())
+            self.map.zoom_to_box(self.map.view_transform().backward(env))
+
+            self.total_scale = 1.0
+            self.scale = False
 
         im = mapnik.Image(self.map.width, self.map.height)
         mapnik.render(self.map, im)
         self.qim.loadFromData(QByteArray(im.tostring('png')))
         self.update()
-
-        # self.envelopeItem.setText(str(self.map.envelope()))
-        # self.widthItem.setText(str(self.map.width))
-        # self.heightItem.setText(str(self.map.height))
 
     #temp functions to try a few things
     def createMap(self):
@@ -84,20 +128,15 @@ class MapnikWidget(QWidget):
         self.map.append_style('Estilo1', s)
 
         #Loading source
-        data_source = mapnik.Shapefile(file = 'Colombia/Colombia.shp')
+        data_source = mapnik.Shapefile(file = '110m-admin-0-countries/ne_110m_admin_0_countries.shp')
         print data_source.envelope()
 
         layer = mapnik.Layer('Colombia')
         layer.datasource = data_source
+        print "\tLayer: ", layer.srs
         layer.styles.append('Estilo1')
 
         self.map.layers.append(layer)
         self.map.zoom_all()
 
         self.updateMap()
-
-        # im = mapnik.Image(self.map.width, self.map.height)
-        # mapnik.render(self.map, im)
-        #
-        # self.qim.loadFromData(QByteArray(im.tostring('png')))
-        # self.update()
